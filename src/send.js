53c20c3f52b38d42143027cda982b6a028c1cfe9
Leon <liwangp2012@163.com>
2019-09-12 18:09:11 +0800

    1

--- src/send.js
+++ src/send.js
@@ -6,15 +6,20 @@ amqp.connect("amqp://leon:root@192.168.99.100", function (err, connection) {
     connection.createChannel(function (err, channel) {
         if (err) throw err;
 
-        let exchange = "direct_logs";
-        let msg = 'Hello publish/subscribe';
-        // let severity = "info";//can be : info/warning/error
-        channel.assertExchange(exchange, "direct", { durable: false });
+        let exchange = 'topic_logs';
+
+
+        channel.assertExchange(exchange, "topic", { durable: false });
+
+
 
         channel.publish(exchange, 'info', Buffer.from("info 1"));
-        channel.publish(exchange, 'warning', Buffer.from("warning 1"));
-        channel.publish(exchange, 'warning', Buffer.from("warning 2"));
-        channel.publish(exchange, 'error', Buffer.from("error 1"));
+        
+
+        // channel.publish(exchange, 'info', Buffer.from("info 1"));
+        // channel.publish(exchange, 'warning', Buffer.from("warning 1"));
+        // channel.publish(exchange, 'warning', Buffer.from("warning 2"));
+        // channel.publish(exchange, 'error', Buffer.from("error 1"));
         // console.log(" [x] Sent %s: '%s'", severity, msg);
 
 
